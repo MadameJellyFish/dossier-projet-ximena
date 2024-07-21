7e82d9199467b5ad1bff0eb4e539faba92eb5add
Ximena Gonzalez <103193801+MadameJellyFish@users.noreply.github.com>
2024-07-21 23:07:48 +0200

    Update script_sql.sql
    docs: fix script with new MCD

--- Specifications-techniques/Merise/script_sql.sql
+++ Specifications-techniques/Merise/script_sql.sql
@@ -1,127 +1,122 @@
+-- PostgreSQL pgAdmin4
+CREATE DATABASE "bads_club" ENCODING "UTF8";
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE Users(
-   user_id UUID DEFAULT uuid_generate_v4() NOT NULL,
-   first_name VARCHAR(50)  NOT NULL,
-   last_name VARCHAR(50)  NOT NULL,
-   email VARCHAR(255)  NOT NULL,
-   password VARCHAR(255)  NOT NULL,
-   birthdate DATE NOT NULL,
-   PRIMARY KEY(user_id),
-   UNIQUE(email)
+CREATE TABLE roles (
+   role_id SERIAL PRIMARY KEY,
+   name VARCHAR(50) NOT NULL UNIQUE
 );
 
-CREATE TABLE Addresses(
-   address_id SERIAL,
-   address VARCHAR(255)  NOT NULL,
-   city VARCHAR(255)  NOT NULL,
-   country VARCHAR(255)  NOT NULL,
-   zip_code VARCHAR(50)  NOT NULL,
-   PRIMARY KEY(address_id)
+CREATE TABLE sports (
+   sport_id SERIAL PRIMARY KEY,
+   name VARCHAR(50) NOT NULL UNIQUE
 );
 
-CREATE TABLE Sports(
-   sport_id SERIAL,
-   sport_name VARCHAR(50)  NOT NULL,
-   PRIMARY KEY(sport_id)
-);
-
-CREATE TABLE Sport_Fields(
-   sport_field_id SERIAL,
-   numero INTEGER NOT NULL,
+CREATE TABLE sports_courts (
+   court_id SERIAL PRIMARY KEY,
+   name VARCHAR(50) NOT NULL UNIQUE,
    sport_id INTEGER NOT NULL,
-   PRIMARY KEY(sport_field_id),
-   UNIQUE(numero),
-   FOREIGN KEY(sport_id) REFERENCES Sports(sport_id)
+   CONSTRAINT fk_sport FOREIGN KEY (sport_id) REFERENCES sports(sport_id)
 );
 
-CREATE TABLE Fields_Availabilities(
-   field_availability_id SERIAL,
-   field_start_time TIMESTAMP NOT NULL,
-   availability_date DATE NOT NULL,
-   sport_field_id INTEGER NOT NULL,
-   PRIMARY KEY(field_availability_id),
-   FOREIGN KEY(sport_field_id) REFERENCES Sport_Fields(sport_field_id)
+CREATE TABLE days_of_week (
+   day_id SERIAL PRIMARY KEY,
+   day VARCHAR(10) NOT NULL UNIQUE
 );
 
-CREATE TABLE Days(
-   day_id SERIAL,
-   day_name VARCHAR(50)  NOT NULL,
-   PRIMARY KEY(day_id),
-   UNIQUE(day_name)
+CREATE TABLE practice_levels (
+   practice_level_id SERIAL PRIMARY KEY,
+   level VARCHAR(20) NOT NULL UNIQUE
 );
 
-CREATE TABLE Admins(
-   admin_id UUID DEFAULT uuid_generate_v4() NOT NULL,
-   admin_first_name VARCHAR(255)  NOT NULL,
-   admin_password VARCHAR(255)  NOT NULL,
-   admin_last_name VARCHAR(255)  NOT NULL,
-   admin_email VARCHAR(255)  NOT NULL,
-   PRIMARY KEY(admin_id),
-   UNIQUE(admin_email)
+CREATE TABLE reservation_status (
+   status_id SERIAL PRIMARY KEY,
+   name VARCHAR(20) NOT NULL UNIQUE
 );
 
-CREATE TABLE Members(
-   member_id INTEGER,
-   photo_url VARCHAR(255) ,
-   has_match_making BOOLEAN NOT NULL,
-   address_id INTEGER NOT NULL,
-   user_id CHAR(36)  NOT NULL,
-   PRIMARY KEY(member_id),
-   UNIQUE(user_id),
-   FOREIGN KEY(address_id) REFERENCES Addresses(address_id),
-   FOREIGN KEY(user_id) REFERENCES Users(user_id)
-);
-
-CREATE TABLE Members_Reservations(
-   reservation_id SERIAL,
-   reservation_date TIMESTAMP NOT NULL,
-   cancelation_date TIMESTAMP,
-   field_availability_id INTEGER NOT NULL,
-   member_id INTEGER NOT NULL,
-   PRIMARY KEY(reservation_id),
-   UNIQUE(field_availability_id),
-   FOREIGN KEY(field_availability_id) REFERENCES Fields_Availabilities(field_availability_id),
-   FOREIGN KEY(member_id) REFERENCES Members(member_id)
-);
-
-CREATE TABLE Members_Availabilities(
-   _availability_id SERIAL,
-   start_time TIMESTAMP NOT NULL,
-   end_time TIMESTAMP NOT NULL,
+CREATE TABLE sports_court_availabilities (
+   sport_availability_id SERIAL PRIMARY KEY,
+   start_time TIME NOT NULL,
+   end_time TIME NOT NULL,
    day_id INTEGER NOT NULL,
-   member_id INTEGER NOT NULL,
-   PRIMARY KEY(_availability_id),
-   FOREIGN KEY(day_id) REFERENCES Days(day_id),
-   FOREIGN KEY(member_id) REFERENCES Members(member_id)
+   court_id INTEGER NOT NULL,
+   CONSTRAINT fk_day FOREIGN KEY (day_id) REFERENCES days_of_week(day_id),
+   CONSTRAINT fk_court FOREIGN KEY (court_id) REFERENCES sports_courts(court_id)
 );
 
-CREATE TABLE Members_Sports(
-   member_id INTEGER,
-   sport_id INTEGER,
-   level VARCHAR(50)  NOT NULL,
-   PRIMARY KEY(member_id, sport_id),
-   UNIQUE(level),
-   FOREIGN KEY(member_id) REFERENCES Members(member_id),
-   FOREIGN KEY(sport_id) REFERENCES Sports(sport_id)
+CREATE TABLE users (
+   user_uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+   last_name VARCHAR(50) NOT NULL,
+   first_name VARCHAR(50) NOT NULL,
+   email VARCHAR(100) NOT NULL UNIQUE,
+   password VARCHAR(255) NOT NULL,
+   membership_start DATE DEFAULT NULL,
+   membership_end DATE DEFAULT NULL,
+   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   role_id INTEGER NOT NULL,
+   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
 
-CREATE TABLE Members_Bans(
-   banned_member_id INTEGER,
-   banned_by_member_id INTEGER,
-   reason VARCHAR(50) ,
-   ban_date TIMESTAMP NOT NULL,
-   PRIMARY KEY(banned_member_id, banned_by_member_id),
-   FOREIGN KEY(banned_member_id) REFERENCES Members(member_id),
-   FOREIGN KEY(banned_by_member_id) REFERENCES Members(member_id)
+CREATE TABLE users_addresses (
+   address_id SERIAL PRIMARY KEY,
+   address VARCHAR(255) NOT NULL,
+   city VARCHAR(255) NOT NULL,
+   country VARCHAR(255) NOT NULL,
+   zipcode VARCHAR(20) NOT NULL,
+   user_uuid UUID NOT NULL,
+   CONSTRAINT fk_user_address FOREIGN KEY(user_uuid) REFERENCES users(user_uuid)
 );
 
-CREATE TABLE Members_Notations(
-   member_id INTEGER,
-   target_member_id INTEGER,
-   commentary VARCHAR(500) ,
-   note INTEGER NOT NULL,
-   PRIMARY KEY(member_id, target_member_id),
-   FOREIGN KEY(member_id) REFERENCES Members(member_id),
-   FOREIGN KEY(target_member_id) REFERENCES Members(member_id)
+CREATE TABLE users_availabilities (
+   user_availability_id SERIAL PRIMARY KEY,
+   start_time TIME NOT NULL,
+   end_time TIME NOT NULL,
+   user_uuid UUID NOT NULL,
+   day_id INTEGER NOT NULL,
+   CONSTRAINT fk_user_availability FOREIGN KEY(user_uuid) REFERENCES users(user_uuid),
+   CONSTRAINT fk_day_availability FOREIGN KEY (day_id) REFERENCES days_of_week(day_id)
+);
+
+CREATE TABLE users_reservations (
+   user_reservation_id SERIAL PRIMARY KEY,
+   reservation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   user_uuid UUID NOT NULL,
+   court_id INTEGER NOT NULL,
+   status_id INTEGER NOT NULL,
+   CONSTRAINT fk_user_reservation FOREIGN KEY(user_uuid) REFERENCES users(user_uuid),
+   CONSTRAINT fk_court_reservation FOREIGN KEY (court_id) REFERENCES sports_courts(court_id),
+   CONSTRAINT fk_status_reservation FOREIGN KEY (status_id) REFERENCES reservation_status(status_id)
+);
+
+CREATE TABLE users_ratings (
+   rated_by_user_id UUID NOT NULL,
+   rated_user_id UUID NOT NULL,
+   rating INTEGER NOT NULL,
+   comment TEXT DEFAULT NULL,
+   rating_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   PRIMARY KEY (rated_by_user_id, rated_user_id),
+   CONSTRAINT fk_rated_by_user FOREIGN KEY (rated_by_user_id) REFERENCES users(user_uuid),
+   CONSTRAINT fk_rated_user FOREIGN KEY (rated_user_id) REFERENCES users(user_uuid)
+);
+
+CREATE TABLE users_bans (
+   banned_by_user_id UUID NOT NULL,
+   banned_user_id UUID NOT NULL,
+   reason TEXT DEFAULT NULL,
+   banned_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   PRIMARY KEY (banned_by_user_id, banned_user_id),
+   CONSTRAINT fk_banned_by_user FOREIGN KEY (banned_by_user_id) REFERENCES users(user_uuid),
+   CONSTRAINT fk_banned_user FOREIGN KEY (banned_user_id) REFERENCES users(user_uuid)
+);
+
+CREATE TABLE user_sports (
+   user_uuid UUID NOT NULL,
+   sport_id INTEGER NOT NULL,
+   practice_level_id INTEGER NOT NULL,
+   PRIMARY KEY(user_uuid, sport_id, practice_level_id),
+   CONSTRAINT fk_user_sport FOREIGN KEY(user_uuid) REFERENCES users(user_uuid),
+   CONSTRAINT fk_sport_user FOREIGN KEY (sport_id) REFERENCES sports(sport_id),
+   CONSTRAINT fk_practice_level_user FOREIGN KEY (practice_level_id) REFERENCES practice_levels(practice_level_id)
 );
